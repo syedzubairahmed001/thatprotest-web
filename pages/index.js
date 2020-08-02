9d521dc070c24cc14b3ded7576abc6b1a94216d1
Syed Zubair Ahmed <syedzubairahmed001@gmail.com>
2020-08-02 12:50:55 +0530

    adds google site verification

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
           content="That Protest is an online cyber protesting platform created in covid
           lockdown."
         />
+        <meta name="google-site-verification" content="GMukjUvzX7N_4jFCWt-Hu2lgJ4Xi1aDF5r0TZiciNHc" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
