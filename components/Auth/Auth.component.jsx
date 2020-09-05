e7b5c6042fad5c91a1c9dcd3bff42b64fafa2a6f
Fakeha Rahman <fakeha126@gmail.com>
2020-09-05 14:34:24 +0530

    Auth modal

--- components/Auth/Auth.component.jsx
+++ components/Auth/Auth.component.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { TextField, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: "60%",
+    // padding: "1rem",
+    // boxSizing: "border-box",
+    margin: "auto"
+  },
+});
 
 const Auth = (props) => {
-  return <div></div>;
+  const styles = useStyles();
+  return (
+    <div className={styles.root}>
+        {/* to display username when its signup mode */}
+      {props.isSignup ? (
+        <TextField label="Username" name="username" onChange={props.changed} />
+      ) : null}
+      <br />
+      <TextField label="Email" name="email" onChange={props.changed} />
+      <br />
+      <TextField
+        label="Password"
+        name="password"
+        type="password"
+        onChange={props.changed}
+      />
+      <br />
+      {/* to display Login and signup based on condition */}
+      <Button onClick={props.submitHandler}>{props.isSignup? 'SIGN UP' : 'LOG IN' }</Button>
+    </div>
+  );
 };
 
 export default Auth;
